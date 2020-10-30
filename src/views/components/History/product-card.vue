169c088b10b66b825f2c446e305064761225ed7f
sheena_arshad <apps.shabeena@gmail.com>
2020-10-30 20:37:17 +0500

    History Layout Changes

--- src/views/components/History/product-card.vue
+++ src/views/components/History/product-card.vue
@@ -1,31 +1,30 @@
 <template>
-  <v-card>
-    <v-img :aspect-ratio="16 / 9" :src="item.image"></v-img>
-
-    <v-card-actions>
-      <v-card-title>{{ item.orderNo }}</v-card-title>
-      <v-spacer></v-spacer>
+   <v-card
+   class="mx-auto"
+    max-width="400"
+   >
+    <v-img
+      :aspect-ratio="16 / 9" :src="item.image"
+      gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.8)"
+    >
+    </v-img>
+    <v-card-actions class="pa-4">
+      <v-avatar color="red">
+        <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+      </v-avatar>
+      <v-card-text>
+      <v-card-title>{{ item.clientName}}</v-card-title>
       <v-card-subtitle>{{ item.orderDate }}</v-card-subtitle>
+      </v-card-text>
+      <v-spacer></v-spacer>
+      <v-chip label class="pa-2"  
+      flat
+      color="orange lighten-2">
+        Label
+      </v-chip>
     </v-card-actions>
-
     <v-divider></v-divider>
     <v-list>
-      <v-list-item>
-        <v-col cols="3" class="pa-0">
-          <v-list-item-action-text>{{ $t("orders.referredBy") }}</v-list-item-action-text>
-        </v-col>
-        <v-list-item-content class="align-end">
-          {{ item.referredBy }}
-        </v-list-item-content>
-      </v-list-item>
-      <v-list-item>
-        <v-col cols="3" class="pa-0">
-          <v-list-item-action-text>{{ $t("orders.clientName") }}</v-list-item-action-text>
-        </v-col>
-        <v-list-item-content class="align-end">
-          {{ item.clientName }}
-        </v-list-item-content>
-      </v-list-item>
       <v-list-item>
         <v-col cols="3" class="pa-0">
           <v-list-item-action-text>{{ $t("orders.productName") }}</v-list-item-action-text>
@@ -33,11 +32,6 @@
         <v-list-item-content>
           {{ item.productName }}
         </v-list-item-content>
-        <!-- <v-list-item-action>
-                    <v-btn icon>
-                      <v-icon color="grey lighten-1">mdi-information</v-icon>
-                    </v-btn>
-                  </v-list-item-action> -->
       </v-list-item>
       <v-list-item>
         <v-col cols="3" class="pa-0">
