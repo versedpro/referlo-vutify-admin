20a08c8baa4b7054d21a5a7412005ef11b642bb6
sheena_arshad <apps.shabeena@gmail.com>
2020-11-20 21:41:21 +0500

    Product Dialogue

--- src/views/components/product1/orders-dialog.vue
+++ src/views/components/product1/orders-dialog.vue
@@ -1,6 +1,21 @@
 <template>
   <v-row justify="center">
     <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+      <v-toolbar
+          dark
+          color="primary"
+        >
+          <v-spacer></v-spacer>
+          <v-toolbar-items>
+            <v-btn
+            icon
+            dark
+            @click="dialog = false"
+          >
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
       <template v-slot:activator="{ on, attrs }">
         <v-list-item two-line>
           <v-list-item-content>
@@ -25,60 +40,96 @@
           </v-toolbar-items>
         </v-toolbar> -->
         <v-img :src="product.src" :aspect-ratio="1.91 / 1" position="top center"></v-img>
-        <v-sheet class="mx-12 mt-12">
-          <v-card-text class="pa-0 mb-6">{{product.productDescription}}</v-card-text>
-          <v-text-field
+        <v-card-text class="pa-0 mb-6 px-4 primary--text">
+          <v-list two-line class="pa-0">
+            <v-list-item class="pa-0">
+              <v-list-item-content class="pa-0">
+                <v-list-item-title v-text="product.productName"></v-list-item-title>
+                <v-list-item-subtitle v-text="product.supplierName"></v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-spacer></v-spacer>
+                <v-list-item-action-text v-text="productPoints"></v-list-item-action-text>
+              </v-list-item-action>
+            </v-list-item>
+            <v-divider class="mb-4 mt-2"></v-divider>
+          </v-list>
+          <p v-html="product.productDescription"></p>
+        </v-card-text>
+        <v-sheet class="mx-4 mt-6">
+          <!-- <v-row>
+            <v-col cols="6">
+            <v-text-field
             v-model="productName"
             :label="$t('referal.productName')"
             disabled
           ></v-text-field>
+            </v-col>
+            <v-col cols="6">
           <v-text-field
             v-model="productSupplierName"
             :label="$t('referal.supplierName')"
             disabled
           ></v-text-field>
+            </v-col>
+          </v-row> 
           <v-text-field
             v-model="productPoints"
             :label="$t('referal.points')"
             disabled
-          ></v-text-field>
-           <v-expansion-panels focusable>
-          <v-expansion-panel>
-            <v-expansion-panel-header color="gold">Want to  make an ORDER!</v-expansion-panel-header>
-            <v-expansion-panel-content>
+          ></v-text-field>-->
+          <v-expansion-panels focusable class="pb-4">
+            <v-expansion-panel>
+              <v-alert
+                border="bottom"
+                color="gold"
+                colored-border
+                dense
+                class="pa-4 ma-0 primary rounded-b-0"
+              >
+                <v-expansion-panel-header hide-actions disable-icon-rotate class="pa-0"
+                  ><v-row class="pa-0 ma-0 d-flex justify-space-between">
+                    <v-icon class="gold--text">mdi-hand-pointing-down</v-icon>
+                    <p class="ma-0 gold--text">Order Now</p>
+                  </v-row>
+                </v-expansion-panel-header>
+              </v-alert>
+              <v-expansion-panel-content>
                 <v-form class="mx-12 pb-8" ref="form" v-model="valid" lazy-validation>
-          <v-text-field
-            v-model="name"
-            :rules="nameRules"
-            :label="$t('referal.clientName')"
-            required
-          ></v-text-field>
+                  <v-text-field
+                    v-model="name"
+                    :rules="nameRules"
+                    :label="$t('referal.clientName')"
+                    required
+                  ></v-text-field>
 
-          <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            :label="$t('referal.email')"
-            required
-          ></v-text-field>
+                  <v-text-field
+                    v-model="email"
+                    :rules="emailRules"
+                    :label="$t('referal.email')"
+                    required
+                  ></v-text-field>
 
-          <v-text-field
-            v-model="phone"
-            :rules="phoneRules"
-            :label="$t('referal.phone')"
-            required
-          ></v-text-field>
+                  <v-text-field
+                    v-model="phone"
+                    :rules="phoneRules"
+                    :label="$t('referal.phone')"
+                    required
+                  ></v-text-field>
 
-          <v-btn :disabled="!valid" color="gold" class="mr-4" @click="submit">Submit</v-btn>
+                  <v-btn :disabled="!valid" color="gold" class="mr-4" @click="submit"><v-icon left> mdi-check-circle </v-icon>Submit</v-btn>
 
-          <v-btn color="gold" class="mr-4" @click="reset"> Reset </v-btn>
+                  <v-btn color="primary" class="mr-4" @click="reset"><v-icon left> mdi-reload</v-icon>Reset </v-btn>
 
-          <v-btn color="primary"  @click="dialog = false">Cancel</v-btn>
-        </v-form>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-      </v-expansion-panels>
+                  <!-- <v-btn color="primary" @click="dialog = false"
+                    ><v-icon left> mdi-close-circle </v-icon>Cancel</v-btn
+                  > -->
+                </v-form>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
         </v-sheet>
-       
+
         <!-- <v-form class="mx-12 pb-8" ref="form" v-model="valid" lazy-validation>
           <v-text-field
             v-model="name"
