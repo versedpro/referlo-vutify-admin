109be25789b145990fd8df199b2e89722e3501f1
sheena_arshad <apps.shabeena@gmail.com>
2020-12-01 22:23:21 +0500

    more button

---
+++ src/router/modules/more.ts
@@ -0,0 +1,60 @@
+/** When your routing table is too long, you can split it into small modules */
+
+import Layout from "@/views/layout/the-layout.vue";
+
+const moreRouter = {
+  path: "/",
+  component: Layout,
+  meta: {
+    title: "route.more",
+    icon: "mdi-dots-horizontal",
+    noCache: true,
+    affix: true
+  },
+  children: [
+    {
+      path: "/about-us",
+      component: () => import("@/views/components/about-us.vue"),
+      name: "About",
+      meta: { title: "route.about", icon: "mdi-translate" }
+    },
+    {
+      path: "/contact-us",
+      component: () => import("@/views/components/contact-us.vue"),
+      name: "Contact",
+      meta: { title: "route.contact", icon: "mdi-translate" }
+    },
+    {
+      path: "/how-to",
+      component: () => import("@/views/components/how-to.vue"),
+      name: "HowtoUse",
+      meta: {
+        title: "route.HowtoUse",
+        icon: "mdi-translate"
+      }
+    },
+    {
+      path: "/latest-news",
+      component: () => import("@/views/components/latest-news.vue"),
+      name: "LatestNews",
+      meta: {
+        title: "route.latestNews",
+        icon: "mdi-translate"
+      }
+    },
+    {
+      path: "/product4",
+      component: () => import("@/views/components/product4/index.vue"),
+      title: "Test4",
+      meta: {
+        title: "Test4",
+        icon: "mdi-timeline-text",
+        noCache: true,
+        affix: true
+      }
+    }
+
+  ]
+};
+
+export default moreRouter;
