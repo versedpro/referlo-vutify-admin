1ea4a43c9a2c99f7c449f1c4ea15cdc5d56d8fa7
taukeer <taukeer.liaqat@gmail.com>
2020-12-04 13:09:28 +0500

    keep alive removed

--- src/views/layout/the-content.vue
+++ src/views/layout/the-content.vue
@@ -2,9 +2,7 @@
   <v-main class="pb-0 grey">
     <v-sheet tile class="mx-auto" max-width="960" height="calc(100vh - 48px">
       <transition name="fade-transform" mode="out-in">
-        <keep-alive>
-          <router-view />
-        </keep-alive>
+        <router-view />
       </transition>
       <!-- <the-bottom-navigation /> -->
     </v-sheet>
